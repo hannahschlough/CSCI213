7b096a4b70fa37571b7df63f079d76a637887540
hannahschlough <hannah.schlough@ndsu.edu>
2023-11-16 16:02:25 -0600

    changed web form

--- Assignment4WebApp/mywork/instructor.aspx.cs
+++ Assignment4WebApp/mywork/instructor.aspx.cs
@@ -9,9 +9,22 @@ namespace Assignment4WebApp.mywork
 {
     public partial class instructor : System.Web.UI.Page
     {
+        /*
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            dbcon = new DepartmentsDataContext(connectionString);
+            ShowAllRecords();
         }
+        DepartmentsDataContext dbcon;
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\16123\\Downloads\\CSCI 213\\SchloughHelpAssign4Tutorial\\CSDepartmentWeb\\App_Data\\NDSUdatabase.mdf\";Integrated Security=True;Connect Timeout=30";
+
+        public void ShowAllRecords()
+        {
+            var records = from item in dbcon.Faculties
+                          select item;
+            GridView1.DataSource = records;
+            GridView1.DataBind();
+        }
+        */
     }
 }
